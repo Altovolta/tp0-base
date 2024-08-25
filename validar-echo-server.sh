b6a31e71cbf20c0dd81747ee6ace8df07f246711
AltovoIta <cavila@fi.uba.ar>
2024-08-25 14:23:42 -0300

    Resuelto ejercicio 3

---
+++ validar-echo-server.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+docker build . -f server_test/Dockerfile -t echo_server_test:latest
+answer=$(docker run --name=echo-test --env-file=server_test/server_config.txt \
+         --network=tp0_testing_net echo_server_test:latest)
+
+if [ "$answer" == "hola" ] 
+then 
+    echo "action: test_echo_server | result: success"
+else 
+    echo "action: test_echo_server | result: fail"
+fi 
+
+docker stop echo-test  >/dev/null && docker rm echo-test  >/dev/null 
+docker rmi echo_server_test:latest > /dev/null
+

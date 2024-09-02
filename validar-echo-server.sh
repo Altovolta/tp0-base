f0299c3dbf8f9d381f796d48449f86866445d0d5
AltovoIta <cavila@fi.uba.ar>
2024-09-02 13:50:58 -0300

    fix ej3

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -2,8 +2,11 @@
 
 docker build . -f server_test/Dockerfile -t echo_server_test:latest
 
+docker network create -d bridge testing_net
+docker network connect testing_net server
+
 answer=$(docker run --rm --name=echo-test --env-file=server_test/server_config.txt \
-         --network=tp0_testing_net echo_server_test:latest)
+         --network=testing_net echo_server_test:latest)
 
 if [ "$answer" = "hola" ] 
 then 
@@ -12,7 +15,7 @@ else
     echo "action: test_echo_server | result: fail"
 fi 
 
-docker container stop echo-test  >/dev/null && docker rm echo-test  >/dev/null 
+docker container stop server  >/dev/null && docker rm server  >/dev/null 
 docker rmi echo_server_test:latest > /dev/null
 docker network rm -f testing_net >/dev/null
 

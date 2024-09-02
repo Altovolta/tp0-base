a40d5b03248a87f66a013ae45d69373e99fbc63f
AltovoIta <cavila@fi.uba.ar>
2024-09-02 16:41:27 -0300

    cambios en script

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -2,11 +2,8 @@
 
 docker build . -f server_test/Dockerfile -t echo_server_test:latest
 
-docker network create -d bridge testing_net
-docker network connect testing_net server
-
 answer=$(docker run --rm --name=echo-test --env-file=server_test/server_config.txt \
-         --network=testing_net echo_server_test:latest)
+         --network=tp0_testing_net echo_server_test:latest)
 
 if [ "$answer" = "hola" ] 
 then 
@@ -14,8 +11,4 @@ then
 else 
     echo "action: test_echo_server | result: fail"
 fi 
-
-docker container stop server  >/dev/null && docker rm server  >/dev/null 
-docker rmi echo_server_test:latest > /dev/null
-docker network rm -f testing_net >/dev/null
 

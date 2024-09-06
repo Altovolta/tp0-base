63a0358e16f8ab66102a5da2dcfce4e05c8b0ff0
AltovoIta <cavila@fi.uba.ar>
2024-09-05 22:29:48 -0300

    Agrego restriccion de 8kB

--- client/common/constants.go
+++ client/common/constants.go
@@ -7,3 +7,8 @@ const ALL_BETS_SENT_CODE = "2"
 // Command code from server
 const BATCH_RECEIVED_SUCCESS = "OK\n"
 const BATCH_RECEIVED_FAILED = "E\n"
+
+// Restrictions
+const MAX_BATCH_BYTE_SIZE = 8000
+const BET_BYTE_SIZE = 59
+const BATCH_HEADER_SIZE = 5 //tipo de mensaje + cantidad de apuestas en el batch
